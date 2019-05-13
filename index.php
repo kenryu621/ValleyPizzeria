6a7d359c853d63b2577ed63ec768de238f9caf6f
Yifan Yu <kenryu422@outlook.com>
2019-05-13 15:23:55 -0700

    Feat: Added footer and text in HOME

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+  require('modules/header.php');
+
+  if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    if (file_exists("content/$page.php")) {
+      include("content/$page.php");
+    } else {
+      echo "<h1>404: Oops! Looks like you\'re lost!</h1>";
+    }
+  } else {
+    include("content/home.php");
+  }
+?>
