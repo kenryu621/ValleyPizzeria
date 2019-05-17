7746ac21317521699666643a20bb225a91036a15
Yifan Yu <kenryu422@outlook.com>
2019-05-17 05:53:39 -0700

    Fixed: PHP CS Fixer

--- index.php
+++ index.php
@@ -2,13 +2,12 @@
   require('modules/header.php');
 
   if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-    if (file_exists("content/$page.php")) {
-      include("content/$page.php");
-    } else {
-      echo "<h1>404: Oops! Looks like you\'re lost!</h1>";
-    }
+      $page = $_GET['page'];
+      if (file_exists("content/$page.php")) {
+          include("content/$page.php");
+      } else {
+          echo "<h1>404: Oops! Looks like you\'re lost!</h1>";
+      }
   } else {
-    include("content/home.php");
+      include("content/home.php");
   }
-?>
