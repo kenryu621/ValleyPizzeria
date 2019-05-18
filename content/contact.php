4385378d1209e0ddd910c6b7db6ecb4d292aad5b
Yifan Yu <kenryu422@outlook.com>
2019-05-17 19:20:37 -0700

    Feat: Added Contact

---
+++ content/contact.php
@@ -0,0 +1,49 @@
+<div class="content" id="contact">
+  <div class="contactLeft">
+    <h3>Address</h3>
+    <h4>551 McCary St Hollister, CA 95023</h4>
+    <h3>Phone Number</h3>
+    <h4>(831)265-7880</h4>
+    <h3>Open Hours</h3>
+    <h4>Mon 11AM - 10PM</h4>
+    <h4>Tue 11AM - 10PM</h4>
+    <h4>Wed 11AM - 10PM</h4>
+    <h4>Thu 11AM - 10PM</h4>
+    <h4>Fri 11AM - 11PM</h4>
+    <h4>Sat 11AM - 11PM</h4>
+    <h4>Sun 11AM - 10PM</h4>
+  </div>
+  <div class="contactCenter">
+    <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7593.531222714748!2d-121.40273665224966!3d36.84998134047828!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8091f62b7327e203%3A0x9f54c091f744d7f4!2sValley+Pizzeria!5e0!3m2!1sen!2sus!4v1558145183489!5m2!1sen!2sus"
+        frameborder="0" style="border:0" allowfullscreen>
+      </iframe></div>
+    <span class="yelp-review" data-review-id="TwyCyI7KStaqi9b1D5BUbQ" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=iXaYFhIheD0WsYlCQ11RMA" rel="nofollow noopener">Nicole G.</a>'s <a href="https://www.yelp.com/biz/valley-pizzeria-hollister?hrid=TwyCyI7KStaqi9b1D5BUbQ"
+        rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/Y9I9Ht3rlvsNDIHFlnOFOQ" rel="nofollow noopener">Valley Pizzeria</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a>
+      <script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script>
+    </span>
+    <span class="yelp-review" data-review-id="QGrXq3L666MqBGViEm67YA" data-hostname="www.yelp.com">Read <a href="https://www.yelp.com/user_details?userid=z7QqCMNya6_a03ntyk7bJQ" rel="nofollow noopener">J O.</a>'s <a href="https://www.yelp.com/biz/valley-pizzeria-hollister?hrid=QGrXq3L666MqBGViEm67YA"
+        rel="nofollow noopener">review</a> of <a href="https://www.yelp.com/biz/Y9I9Ht3rlvsNDIHFlnOFOQ" rel="nofollow noopener">Valley Pizzeria</a> on <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a>
+      <script async="async" src="https://www.yelp.com/embed/widgets.js" type="text/javascript"></script>
+    </span>
+  </div>
+  <div class="contactRight">
+    <h3>More Business Info</h3>
+    <h4>Takes Reservations <span>No</span></h4>
+    <h4>Delivery & Take-out <span>Yes</span></h4>
+    <h4>Accepts Credit Cards <span>Yes</span></h4>
+    <h4>Apple Pay / Google Pay <span>No</span></h4>
+    <h4>Parking <span>Private Lot</span></h4>
+    <h4>Bike Parking <span>No</span></h4>
+    <h4>Wheelchair Accessible <span>Yes</span></h4>
+    <h4><span>Good</span> for Kids and Groups</h4>
+    <h4>Ambience <span>Casual</span></h4>
+    <h4>Noise Level <span>Average</span></h4>
+    <h4>Alcohol <span>Beer & Wine Only</span></h4>
+    <h4>Outdoor Seating <span>No</span></h4>
+    <h4>Wi-Fi <span>Free</span></h4>
+    <h4>Dogs Allowed <span>No</span></h4>
+    <h4>Waiter Service <span>No</span></h4>
+    <h4>Caters <span>Yes</span></h4>
+    <h4>Offers Military Discount <span>Yes</span></h4>
+  </div>
+</div>
